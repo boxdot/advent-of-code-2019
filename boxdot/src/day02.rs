1a0f0cd97a5223154ad7918fbfe427db369d484f
boxdot <d@zerovolt.org>
2019-12-02 12:29:36 +0100

    Make shorter.

--- boxdot/src/day02.rs
+++ boxdot/src/day02.rs
@@ -1,55 +1,36 @@
 use itertools::iproduct;
 
-pub fn solve(input: &str) -> (i64, Option<i64>) {
-    let state = parse(input);
+pub fn solve(input: &str) -> (Option<usize>, Option<usize>) {
+    let memory = parse(input);
 
-    let part1 = {
-        let mut state = state.clone();
-        state[1] = 12;
-        state[2] = 2;
-        execute(state)
-    };
-
+    let part1 = execute(memory.clone(), 12, 2).ok();
     let part2 = iproduct!(0..100, 0..100)
-        .find(|&(noun, verb)| {
-            let mut state = state.clone();
-            state[1] = noun;
-            state[2] = verb;
-            execute(state) == 19_690_720
-        })
+        .find(|&(noun, verb)| execute(memory.clone(), noun, verb) == Ok(19_690_720))
         .map(|(noun, verb)| 100 * noun + verb);
 
     (part1, part2)
 }
 
-fn parse(input: &str) -> Vec<i64> {
+fn parse(input: &str) -> Vec<usize> {
     input.split(',').filter_map(|s| s.parse().ok()).collect()
 }
 
-fn execute(mut state: Vec<i64>) -> i64 {
-    let mut ip = 0; // instruction pointer
-    loop {
-        match state[ip] {
-            1 => {
-                let a = state[state[ip + 1] as usize];
-                let b = state[state[ip + 2] as usize];
-                let addr = state[ip + 3] as usize;
-                state[addr] = a + b;
-                ip += 4;
-            }
-            2 => {
-                let a = state[state[ip + 1] as usize];
-                let b = state[state[ip + 2] as usize];
-                let addr = state[ip + 3] as usize;
-                state[addr] = a * b;
-                ip += 4;
-            }
-            99 => break,
-            other => panic!("invalid opcode: {}", other),
+fn execute(mut mem: Vec<usize>, noun: usize, verb: usize) -> Result<usize, ()> {
+    mem[1] = noun;
+    mem[2] = verb;
+
+    let add = |a, b, c, mem: &mut [usize]| mem[c] = mem[a] + mem[b];
+    let mul = |a, b, c, mem: &mut [usize]| mem[c] = mem[a] * mem[b];
+
+    for ip in (0..).step_by(4) {
+        match mem[ip] {
+            1 => add(mem[ip + 1], mem[ip + 2], mem[ip + 3], &mut mem),
+            2 => mul(mem[ip + 1], mem[ip + 2], mem[ip + 3], &mut mem),
+            99 => return Ok(mem[0]),
+            _ => break,
         }
     }
-
-    state[0]
+    Err(())
 }
 
 #[cfg(test)]
@@ -58,14 +39,9 @@ mod tests {
 
     #[test]
     fn test_execute() {
-        // assert_eq!(
-        //     execute(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]),
-        //     3500
-        // );
-        // assert_eq!(execute(vec![1, 0, 0, 0, 99]), 2);
-        // assert_eq!(execute(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]), 30);
-
-        execute(vec![2, 3, 0, 3, 99]);
-        execute(vec![2, 4, 4, 5, 99, 0]);
+        assert_eq!(
+            execute(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], 12, 2),
+            Ok(3500)
+        );
     }
 }
