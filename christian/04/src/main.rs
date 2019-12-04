b8dac77118407d6531590be2b1341ded30cecea2
Christian Vetter <christian.vetter@here.com>
2019-12-04 15:29:13 +0100

    Beautify a bit

--- christian/04/src/main.rs
+++ christian/04/src/main.rs
@@ -1,32 +1,23 @@
 use itertools::*;
+use std::cmp::Ordering as Ord;
 
-fn digits(x: usize) -> impl Iterator<Item = usize> + Clone {
-    std::iter::successors(Some(x), |x| Some(x / 10).filter(|x| *x != 0)).map(|x| x % 10)
-}
-
-fn filter1(x: &(impl Iterator<Item = usize> + Clone)) -> bool {
-    x.clone().tuple_windows().filter(|(a, b)| a == b).count() != 0
-        && x.clone().tuple_windows().filter(|(a, b)| a < b).count() == 0
-}
-
-fn solve1(range: std::ops::RangeInclusive<usize>) -> usize {
-    range.map(digits).filter(filter1).count()
-}
-
-fn filter2(x: &(impl Iterator<Item = usize> + Clone)) -> bool {
-    for (_, group) in &x.clone().group_by(|d| *d) {
-        if group.count() == 2 {
-            return x.clone().tuple_windows().filter(|(a, b)| a < b).count() == 0;
+fn valid(x: usize, run_filter: &[Ord]) -> bool {
+    let digits =
+        std::iter::successors(Some(x), |x| Some(x / 10).filter(|x| *x != 0)).map(|x| x % 10);
+    for (_, group) in &digits.clone().group_by(|d| *d) {
+        if run_filter.contains(&group.count().cmp(&2)) {
+            return digits.tuple_windows().filter(|(a, b)| a < b).count() == 0;
         }
     }
     false
 }
 
-fn solve2(range: std::ops::RangeInclusive<usize>) -> usize {
-    range.map(digits).filter(filter2).count()
+fn solve(range: std::ops::RangeInclusive<usize>, run_filter: &[Ord]) -> usize {
+    range.filter(|&x| valid(x, run_filter)).count()
 }
 
 fn main() {
-    println!("Part1: {:?}", solve1(124075..=580769));
-    println!("Part2: {:?}", solve2(124075..=580769));
+    let r = 124075..=580769;
+    println!("Part1: {:?}", solve(r.clone(), &[Ord::Equal, Ord::Greater]));
+    println!("Part2: {:?}", solve(r, &[Ord::Equal]));
 }
