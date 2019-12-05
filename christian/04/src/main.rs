007505f2c7dcb550b8b325387e83ad500a2c7b81
Christian Vetter <christian.vetter@here.com>
2019-12-06 00:06:18 +0100

    Moree beautification

--- christian/04/src/main.rs
+++ christian/04/src/main.rs
@@ -37,9 +37,9 @@ fn compute(upper_bound: u64, part1: bool) -> usize {
     }
     let num_digits: usize = bound_digit_count.iter().sum();
 
-    // state space is: digit, digits left, had_run, below_bound
+    // state space is: digit, digits left, had_run, is_less
     let mut state = vec![0; 2 * 2 * 11 * (num_digits + 1)];
-    let index = |digit: Option<usize>, left: usize, had_run: bool, below_bound: bool| {
+    let index = |digit: Option<usize>, left: usize, had_run: bool, is_less: bool| {
         if left > num_digits {
             None
         } else {
@@ -47,57 +47,37 @@ fn compute(upper_bound: u64, part1: bool) -> usize {
                 digit.map(|x| x + 1).unwrap_or(0) * 2 * 2 * (num_digits + 1)
                     + left * 2 * 2
                     + 2 * had_run as usize
-                    + below_bound as usize,
+                    + is_less as usize,
             )
         }
     };
-    let try_get =
-        |state: &[usize], index: Option<usize>| index.map(|index| state[index]).unwrap_or(0);
 
     state[index(None, num_digits, false, false).unwrap()] = 1;
 
-    for digit in 0..=9_usize {
-        for left in 0..=num_digits {
-            for &had_run in [false, true].into_iter() {
-                for &below_bound in [false, true].into_iter() {
-                    let prev_digit = digit.checked_sub(1);
-                    let mut sum = 0;
-
-                    // how many used digits do we want to put into current digit?
-                    for count in 0..=(num_digits - left) {
-                        // if we do not want a run in this state, make sure we do not get one
-                        if !had_run && ((part1 && count > 1) || count == 2) {
-                            continue;
-                        }
-                        // if we do not want to be less than the bound, make sure we are not
-                        if !below_bound && count != bound_digit_count[digit] {
-                            continue;
-                        }
-                        // try all possible state transitions that are allowed
-                        if !below_bound || count > bound_digit_count[digit] {
-                            if had_run {
-                                sum +=
-                                    try_get(&state, index(prev_digit, left + count, true, false));
-                            }
-                            if !had_run || (part1 && count >= 2) || count == 2 {
-                                sum +=
-                                    try_get(&state, index(prev_digit, left + count, false, false));
-                            }
-                        }
-                        if below_bound {
-                            if had_run {
-                                sum += try_get(&state, index(prev_digit, left + count, true, true));
-                            }
-                            if !had_run || (part1 && count >= 2) || count == 2 {
-                                sum +=
-                                    try_get(&state, index(prev_digit, left + count, false, true));
-                            }
-                        }
-                    }
-                    state[index(Some(digit), left, had_run, below_bound).unwrap()] = sum;
-                }
-            }
-        }
+    for (digit, left, &had_run, &is_less) in
+        iproduct!(0..=9, 0..=num_digits, &[false, true], &[false, true])
+    {
+        // how many used digits do we want to put into current digit?
+        // if we do not want a run in this state, make sure we do not get one
+        // if we do not want to be less than the bound, make sure we are not
+        let possible_counts = (0..=(num_digits - left)).filter(|&count| {
+            !(!had_run && ((part1 && count >= 2) || count == 2)
+                || !is_less && count != bound_digit_count[digit])
+        });
+        state[index(Some(digit), left, had_run, is_less).unwrap()] = possible_counts
+            .map(|count| -> usize {
+                // try all possible state transitions that are allowed
+                let prev_is_less = [is_less, is_less && count <= bound_digit_count[digit]];
+                let prev_had_run = [had_run, had_run && !((part1 && count >= 2) || count == 2)];
+                iproduct!(prev_is_less.iter().dedup(), prev_had_run.iter().dedup())
+                    .filter_map(|(&prev_is_less, &prev_had_run)| {
+                        let prev = digit.checked_sub(1);
+                        index(prev, left + count, prev_had_run, prev_is_less)
+                            .map(|index| state[index])
+                    })
+                    .sum()
+            })
+            .sum();
     }
 
     state[index(Some(9), 0, true, true).unwrap()]
@@ -106,17 +86,11 @@ fn compute(upper_bound: u64, part1: bool) -> usize {
 fn main() {
     let r = 124075..=580769;
 
+    println!("Part1: {}", compute(580769, true) - compute(124075, true));
     println!(
-        "Compute Part1: {}",
-        compute(580769, true) - compute(124075, true)
-    );
-    println!(
-        "Check(slow) Part1: {:?}",
+        "Check Part1: {:?}",
         solve(r.clone(), &[Ord::Equal, Ord::Greater])
     );
-    println!(
-        "Compute Part2: {}",
-        compute(580769, false) - compute(124075, false)
-    );
-    println!("Check (slow) Part2: {:?}", solve(r, &[Ord::Equal]));
+    println!("Part2: {}", compute(580769, false) - compute(124075, false));
+    println!("Check Part2: {:?}", solve(r, &[Ord::Equal]));
 }
