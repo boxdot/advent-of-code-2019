e8a92a50a16b928c880603326769b329adc34328
Christian Vetter <christian.vetter@here.com>
2019-12-08 08:43:18 +0100

    Shorten

--- christian/08/src/main.rs
+++ christian/08/src/main.rs
@@ -7,22 +7,18 @@ fn parse(input: &Vec<String>) -> Vec<u8> {
 }
 
 fn solve1(input: &Vec<u8>) -> usize {
-    let layers = input.chunks(25 * 6);
     let count = |x: u8| move |l: &&[u8]| l.iter().filter(move |&&y| y == x).count();
-    let best = layers.min_by_key(count(0)).unwrap();
+    let best = input.chunks(25 * 6).min_by_key(count(0)).unwrap();
     count(1)(&best) * count(2)(&best)
 }
 
 fn solve2(input: &Vec<u8>) -> String {
-    let mut result = String::new();
-    for y in 0..6 {
-        for x in 0..25 {
-            let pixels = input.iter().skip(x + 25 * y).step_by(25 * 6);
-            result.push((b' ' + pixels.filter(|&&p| p != 2).next().unwrap()) as char);
-        }
-        result.push('\n');
-    }
-    result
+    iproduct!(0..6, 0..25).fold(String::new(), |mut res, (x, y)| {
+        let pixels = input.iter().skip(x + 25 * y).step_by(25 * 6);
+        res.push((b' ' + pixels.filter(|&&p| p != 2).next().unwrap()) as char);
+        res.push(if x == 24 { '\n' } else { '‎' });
+        res
+    })
 }
 
 fn main() {
