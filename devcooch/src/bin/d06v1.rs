b36222a694cab9b952fdf8ce0fd8ce764bc285eb
Dmitry Kochkin <cooch@mail.ru>
2019-12-10 01:47:46 +0100

    Day 06

---
+++ devcooch/src/bin/d06v1.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+fn main() {
+    let contents = include_str!("day06.txt");
+    let pairs: Vec<_> = contents
+        .lines()
+        .map(|l| l.split(")"))
+        .map(|mut s| (s.next().unwrap(), s.next().unwrap()))
+        .collect();
+    let rel: HashMap<&str, &str> = pairs.iter().cloned().map(|(x, y)| (y, x)).collect();
+    let mut tot = 0;
+    for mut k in rel.keys() {
+        while rel.contains_key(k) {
+            tot += 1;
+            k = &rel[k];
+        }
+    }
+    println!("{}", tot);
+}
---
+++ devcooch/src/bin/d06v2.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+fn get_orbit<'a>(key: &'a str, rel: &HashMap<&'a str, &'a str>) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    let mut it = key;
+    while rel.contains_key(it) {
+        result.push(it);
+        it = &rel[it];
+    }
+    result.reverse();
+    result
+}
+
+fn main() {
+    let contents = include_str!("day06.txt");
+    let pairs: Vec<_> = contents
+        .lines()
+        .map(|l| l.split(")"))
+        .map(|mut s| (s.next().unwrap(), s.next().unwrap()))
+        .collect();
+    let rel: HashMap<&str, &str> = pairs.iter().cloned().map(|(x, y)| (y, x)).collect();
+    let my = get_orbit("YOU", &rel);
+    let santa = get_orbit("SAN", &rel);
+    let same = my.iter().zip(&santa).filter(|p| *p.0 == *p.1).count();
+    println!("{} {}", my.len(), santa.len());
+    let jumps = my.len() + santa.len() - 2 * same - 2;
+    println!("{}", jumps);
+}
