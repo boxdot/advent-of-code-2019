c6ad209cecb0bdb4cee3e17d5056a6af68a51d9e
boxdot <d@zerovolt.org>
2019-12-15 13:06:06 +0100

    boxdot's day 14, part 2

--- boxdot/src/day14.rs
+++ boxdot/src/day14.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 
-pub fn solve(input: &str) -> isize {
+pub fn solve(input: &str) -> (usize, usize) {
     let rules = parse(input);
-    fuel(&rules)
+    let part1 = ore(&rules, 1);
+    let part2 = optimal_fuel(&rules);
+    (part1, part2)
 }
 
 #[derive(Debug, Hash, PartialEq, Eq)]
@@ -43,9 +45,9 @@ fn parse(input: &str) -> Rules {
         .collect()
 }
 
-fn fuel(rules: &Rules) -> isize {
+fn ore(rules: &Rules, fuel_amount: usize) -> usize {
     let mut amounts = HashMap::new();
-    amounts.insert("FUEL".to_string(), 1);
+    amounts.insert("FUEL".to_string(), fuel_amount as isize);
 
     while let Some((label, amount)) = amounts
         .iter()
@@ -62,7 +64,26 @@ fn fuel(rules: &Rules) -> isize {
         }
     }
 
-    *amounts.get("ORE").unwrap()
+    *amounts.get("ORE").unwrap() as usize
+}
+
+fn optimal_fuel(rules: &Rules) -> usize {
+    const MAX_ORE: usize = 1_000_000_000_000;
+    let mut min = 1;
+    let mut max = MAX_ORE;
+    // binary search for the optimal amount of fuel
+    while min + 1 < max {
+        let mid = (min + max) / 2;
+        let ore = ore(rules, mid);
+        if ore < MAX_ORE {
+            min = mid;
+        } else if ore > MAX_ORE {
+            max = mid;
+        } else {
+            return mid;
+        }
+    }
+    min
 }
 
 #[cfg(test)]
@@ -70,7 +91,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_fuel() {
+    fn test_ore() {
         let rules = parse(
             r#"10 ORE => 10 A
 1 ORE => 1 B
@@ -79,7 +100,7 @@ mod tests {
 7 A, 1 D => 1 E
 7 A, 1 E => 1 FUEL"#,
         );
-        assert_eq!(fuel(&rules), 31);
+        assert_eq!(ore(&rules, 1), 31);
 
         let rules = parse(
             r#"9 ORE => 2 A
@@ -90,7 +111,7 @@ mod tests {
 4 C, 1 A => 1 CA
 2 AB, 3 BC, 4 CA => 1 FUEL"#,
         );
-        assert_eq!(fuel(&rules), 165);
+        assert_eq!(ore(&rules, 1), 165);
 
         let rules = parse(
             r#"157 ORE => 5 NZVS
@@ -103,7 +124,7 @@ mod tests {
 165 ORE => 2 GPVTF
 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"#,
         );
-        assert_eq!(fuel(&rules), 13312);
+        assert_eq!(ore(&rules, 1), 13312);
 
         let rules = parse(
             r#"2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
@@ -119,7 +140,7 @@ mod tests {
 1 VJHF, 6 MNCFX => 4 RFSQX
 176 ORE => 6 VJHF"#,
         );
-        assert_eq!(fuel(&rules), 180697);
+        assert_eq!(ore(&rules, 1), 180697);
 
         let rules = parse(
             r#"171 ORE => 8 CNZTR
@@ -140,6 +161,59 @@ mod tests {
 7 XCVML => 6 RJRHP
 5 BHXH, 4 VRPVC => 5 LTCX"#,
         );
-        assert_eq!(fuel(&rules), 2210736);
+        assert_eq!(ore(&rules, 1), 2210736);
+    }
+
+    #[test]
+    fn test_optimal_fuel() {
+        let rules = parse(
+            r#"157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"#,
+        );
+        assert_eq!(optimal_fuel(&rules), 82892753);
+
+        let rules = parse(
+            r#"2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF"#,
+        );
+        assert_eq!(optimal_fuel(&rules), 5586022);
+
+        let rules = parse(
+            r#"171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX"#,
+        );
+        assert_eq!(optimal_fuel(&rules), 460664);
     }
 }
