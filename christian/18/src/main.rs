430edc3ae49f1409222d7f6e6fc8398a76fcaee3
Christian Vetter <christian.vetter@here.com>
2019-12-18 18:35:20 +0100

    More speed

--- christian/18/src/main.rs
+++ christian/18/src/main.rs
@@ -8,8 +8,10 @@ fn parse(input: &Vec<String>) -> (Vec<u8>, u16) {
     )
 }
 
-fn num_keys(map: &Vec<u8>) -> usize {
-    map.iter().filter(|&&x| b'a' <= x && x <= b'z').count()
+fn all_keys(map: &Vec<u8>) -> u32 {
+    map.iter()
+        .filter(|&&x| b'a' <= x && x <= b'z')
+        .fold(0, |s, c| s | 1 << (c - b'a'))
 }
 
 fn find_start(map: &Vec<u8>) -> u16 {
@@ -21,16 +23,11 @@ fn neighbours(pos: u16, width: u16) -> impl Iterator<Item = u16> {
 }
 
 fn solve1((map, width): (Vec<u8>, u16)) -> Option<usize> {
-    let num_needed = num_keys(&map);
+    let keys_needed = all_keys(&map);
     let start = find_start(&map);
-
-    let mut seen = HashSet::new();
-    let mut stack = VecDeque::new();
-    stack.push_back((start, 0_u32, 0));
+    let mut seen: HashSet<_> = [(start, 0_u32)].iter().cloned().collect();
+    let mut stack: VecDeque<_> = [(start, 0_u32, 0)].iter().cloned().collect();
     while let Some((pos, keys, dist)) = stack.pop_front() {
-        if keys.count_ones() as usize == num_needed {
-            return Some(dist);
-        }
         for new_pos in neighbours(pos, width) {
             let mut new_keys = keys;
             let c = map[new_pos as usize];
@@ -42,6 +39,9 @@ fn solve1((map, width): (Vec<u8>, u16)) -> Option<usize> {
             }
             if (b'a'..=b'z').contains(&c) {
                 new_keys |= 1 << (c - b'a');
+                if new_keys == keys_needed {
+                    return Some(dist + 1);
+                }
             }
             if seen.insert((new_pos, new_keys)) {
                 stack.push_back((new_pos, new_keys, dist + 1));
@@ -52,7 +52,7 @@ fn solve1((map, width): (Vec<u8>, u16)) -> Option<usize> {
 }
 
 fn solve2((mut map, width): (Vec<u8>, u16)) -> Option<usize> {
-    let num_needed = num_keys(&map);
+    let keys_needed = all_keys(&map);
     let start = find_start(&map);
 
     for pos in neighbours(start, width) {
@@ -71,11 +71,7 @@ fn solve2((mut map, width): (Vec<u8>, u16)) -> Option<usize> {
         stack.push_back((current, starts, 0_u32, 0));
         seen.insert((current, starts, 0));
     }
-
     while let Some((i, pos, keys, dist)) = stack.pop_front() {
-        if keys.count_ones() as usize == num_needed {
-            return Some(dist);
-        }
         for coord in neighbours(pos[i as usize], width) {
             let mut new_keys = keys;
             let c = map[coord as usize];
@@ -85,9 +81,12 @@ fn solve2((mut map, width): (Vec<u8>, u16)) -> Option<usize> {
             if (b'A'..=b'Z').contains(&c) && (keys & (1 << (c - b'A')) == 0) {
                 continue;
             }
-            let has_key = (b'a'..=b'z').contains(&c) && (new_keys & 1 << (c - b'a')) == 0;
+            let has_key = (b'a'..=b'z').contains(&c) && (keys & 1 << (c - b'a')) == 0;
             if has_key {
                 new_keys |= 1 << (c - b'a');
+                if new_keys == keys_needed {
+                    return Some(dist + 1);
+                }
             }
             let mut new_pos = pos.clone();
             new_pos[i as usize] = coord;
