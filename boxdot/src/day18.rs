5f2373cc414727fac946e70eaa258b9f477b01f8
boxdot <d@zerovolt.org>
2019-12-21 23:31:32 +0100

    boxdot's day 18, part 2

--- boxdot/src/day18.rs
+++ boxdot/src/day18.rs
@@ -1,96 +1,198 @@
+use std::cmp::Reverse;
 use std::collections::hash_map::Entry;
-use std::collections::{HashMap, VecDeque};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 
-pub fn solve(input: &str) -> Option<usize> {
+pub fn solve(input: &str) -> Option<(usize, usize)> {
     let vault = parse(input);
-    shortest_path_len(&vault)
+    let part1 = shortest_path_len(&vault)?;
+    let part2 = shortest_path_len4(vault)?;
+    Some((part1, part2))
 }
 
 struct Vault {
-    map: Vec<char>,
+    map: Vec<u8>,
     width: usize,
 }
 
 fn parse(input: &str) -> Vault {
     let width = input.lines().next().unwrap().len();
-    let map: Vec<_> = input.lines().flat_map(|l| l.chars()).collect();
+    let map: Vec<_> = input.lines().flat_map(|l| l.bytes()).collect();
     Vault { map, width }
 }
 
-type Coord = (usize, usize);
+type Coord = (u8, u8);
 
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
-struct State {
-    pos: Coord,
-    missing_keys: u32,
+struct PathFinder<'a> {
+    vault: &'a Vault,
+    q: VecDeque<(Coord, usize, u32)>,
+    seen: HashSet<Coord>,
 }
 
-impl State {
-    fn can_open(&self, a: char) -> bool {
-        let bit = 1 << (a as u8 - b'A') as usize;
-        self.missing_keys & bit == 0
+impl<'a> PathFinder<'a> {
+    fn new(vault: &'a Vault) -> Self {
+        Self {
+            vault,
+            q: Default::default(),
+            seen: Default::default(),
+        }
     }
 
-    fn add_key(&mut self, a: char) {
-        let bit = !(1 << (a as u8 - b'a') as usize);
-        self.missing_keys &= bit;
-    }
+    /// Finds all paths from `orig` to keys.
+    ///
+    /// Returns the list of (dest, dist, key, doors on the path).
+    fn bfs(&mut self, orig: Coord, keys: &HashMap<Coord, u32>) -> Vec<(Coord, usize, u32, u32)> {
+        let width = self.vault.width;
+        let index = |(x, y)| x as usize + width * y as usize;
 
-    fn add_missing_key(&mut self, a: char) {
-        let bit = 1 << (a as u8 - b'a') as usize;
-        self.missing_keys |= bit;
+        self.q.clear();
+        self.q.push_back((orig, 0, 0));
+        self.seen.clear();
+        self.seen.insert(orig);
+
+        let mut paths = Vec::new();
+        while let Some((pos, dist, doors)) = self.q.pop_front() {
+            if let Some(&key) = keys.get(&pos) {
+                paths.push((pos, dist, key, doors));
+            }
+
+            let (x, y) = pos;
+            let neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+            for &pos in neighbors.iter() {
+                if self.seen.insert(pos) {
+                    let field = self.vault.map[index(pos)];
+                    match field {
+                        b'#' => continue,
+                        b'.' | b'@' => {
+                            self.q.push_back((pos, dist + 1, doors));
+                        }
+                        a if (a as char).is_lowercase() => {
+                            self.q.push_back((pos, dist + 1, doors));
+                        }
+                        a if (a as char).is_uppercase() => {
+                            let door = 1 << (a - b'A');
+                            self.q.push_back((pos, dist + 1, doors | door))
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+            }
+        }
+
+        paths
     }
 }
 
-// length of the shortest path to collect all keys and open all doors
 fn shortest_path_len(vault: &Vault) -> Option<usize> {
-    let index = |(x, y)| x + vault.width * y;
+    let width = vault.width;
+    let coord = |i| ((i % width) as u8, (i / width) as u8);
 
-    let mut state = State::default();
-    for (i, field) in vault.map.iter().copied().enumerate() {
-        match field {
-            '@' => state.pos = (i % vault.width, i / vault.width),
-            '#' | '.' => (),
-            a if a.is_lowercase() => state.add_missing_key(a),
-            _ => (),
-        };
+    let orig = vault.map.iter().position(|&v| v == b'@').map(coord)?;
+    let keys: HashMap<_, _> = vault
+        .map
+        .iter()
+        .enumerate()
+        .filter(|(_, &v)| (v as char).is_lowercase())
+        .map(|(i, &v)| (coord(i), 1_u32 << (v - b'a')))
+        .collect();
+
+    // precompute all distances from each key to each key with doors on the way
+    let mut path_finder = PathFinder::new(vault);
+    let paths: HashMap<_, _> = keys
+        .keys()
+        .chain([orig].iter())
+        .map(|&from| (from, path_finder.bfs(from, &keys)))
+        .collect();
+
+    let keys_bitset = (1 << keys.len()) - 1;
+    dijkstra([orig], &paths, keys_bitset)
+}
+
+fn shortest_path_len4(mut vault: Vault) -> Option<usize> {
+    let width = vault.width;
+    let coord = |i| ((i % width) as u8, (i / width) as u8);
+    let index = |(x, y)| x as usize + width * y as usize;
+
+    let orig = vault.map.iter().position(|&v| v == b'@').map(coord)?;
+    let keys: HashMap<_, _> = vault
+        .map
+        .iter()
+        .enumerate()
+        .filter(|(_, &v)| (v as char).is_lowercase())
+        .map(|(i, &v)| (coord(i), 1_u32 << (v - b'a')))
+        .collect();
+
+    // add 4 robots and walls around them
+    let (x, y) = orig;
+    for &pos in [(x, y), (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)].iter() {
+        vault.map[index(pos)] = b'#';
     }
+    let origins = [
+        (x - 1, y - 1),
+        (x + 1, y - 1),
+        (x + 1, y + 1),
+        (x - 1, y + 1),
+    ];
 
-    let mut q = VecDeque::new();
-    q.push_back(state);
+    // precompute all distances from each key to each key with doors on the way
+    let mut path_finder = PathFinder::new(&vault);
+    let paths: HashMap<_, _> = keys
+        .keys()
+        .chain(origins.iter())
+        .map(|&from| (from, path_finder.bfs(from, &keys)))
+        .collect();
+
+    let keys_bitset = (1 << keys.len()) - 1;
+    dijkstra(origins, &paths, keys_bitset)
+}
+
+// Length of the shortest path to collect all keys and open all doors.
+//
+// We run Dijkstra on the set of all precomputed paths from origins + keys to all keys and check if
+// the path if traversable by looking at the doors along it and the keys we've already collected.
+fn dijkstra<Ar>(
+    orig: Ar,
+    paths: &HashMap<Coord, Vec<(Coord, usize, u32, u32)>>,
+    keys: u32,
+) -> Option<usize>
+where
+    Ar: AsRef<[Coord]> + AsMut<[Coord]> + Copy + Eq + Ord + std::hash::Hash,
+{
+    let mut heap = BinaryHeap::new();
+    heap.push(Reverse((0, orig, 0)));
 
     let mut distances = HashMap::new();
-    distances.insert(state, 0);
+    distances.insert((orig, 0), 0);
 
-    while let Some(state) = q.pop_front() {
-        let dist = distances[&state];
-        if state.missing_keys == 0 {
+    while let Some(Reverse((dist, positions, collected_keys))) = heap.pop() {
+        if collected_keys == keys {
             return Some(dist);
         }
 
-        let (x, y) = state.pos;
-        let neightbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
-        for pos in neightbors.iter().copied() {
-            let field = vault.map[index(pos)];
-            match field {
-                '#' => continue,
-                a => {
-                    let mut next_state = State {
-                        pos,
-                        missing_keys: state.missing_keys,
-                    };
-                    if a != '.' && a != '@' {
-                        if a.is_uppercase() && !state.can_open(a) {
-                            // we don't have the key yet
-                            continue;
-                        } else if a.is_lowercase() {
-                            next_state.add_key(a);
-                        }
+        for (i, pos) in positions.as_ref().iter().enumerate() {
+            let reachabled_keys =
+                paths[&pos]
+                    .iter()
+                    .copied()
+                    .filter(|&(_dest, _dist, key, doors)| {
+                        collected_keys & key == 0 && !collected_keys & doors == 0
+                    });
+
+            for (dest, key_dist, key, _) in reachabled_keys {
+                let collected_keys = collected_keys | key;
+                let dist = dist + key_dist;
+                let mut positions = positions;
+                positions.as_mut()[i] = dest;
+
+                match distances.entry((positions, collected_keys)) {
+                    Entry::Vacant(entry) => {
+                        entry.insert(dist);
+                        heap.push(Reverse((dist, positions, collected_keys)));
                     }
-                    if let Entry::Vacant(entry) = distances.entry(next_state) {
-                        entry.insert(dist + 1);
-                        q.push_back(next_state);
+                    Entry::Occupied(mut entry) if dist < *entry.get() => {
+                        entry.insert(dist);
+                        heap.push(Reverse((dist, positions, collected_keys)));
                     }
+                    _ => {}
                 }
             }
         }
@@ -152,5 +254,19 @@ mod tests {
 ########################"#,
         );
         assert_eq!(shortest_path_len(&vault), Some(81));
+    }
+
+    #[test]
+    fn test_shortest_path_len4() {
+        let vault = parse(
+            r#"#######
+#a.#Cd#
+##...##
+##.@.##
+##...##
+#cB#Ab#
+#######"#,
+        );
+        assert_eq!(shortest_path_len4(vault), Some(8));
     }
 }
